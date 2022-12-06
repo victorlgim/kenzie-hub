190b0bce666478e50fb5d7c1635f766e8b472bb8
victorglim <victor.guedes888@gmail.com>
2022-12-05 21:52:45 -0300

    refactor: removed authentication props in the header, as well as updating loaders

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,4 @@
 import { useContext } from "react";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { DivLoginPrincipal } from "../../components/FormLogin/style";
 import Header from "../../components/Header/Header";
 import { BaseLoading, LoadingRing } from "../../components/Header/style";
@@ -8,24 +6,21 @@ import ModalAdd from "../../components/ModalAdd/ModalAdd";
 import ModalDelete from "../../components/ModalDelete/ModalDelete";
 import SectionDashboard from "../../components/SectionDashboard/SectionDashboard";
 import SectionPrincipal from "../../components/SectionPrincipal/SectionPrincipal";
+import { GlobalContext } from "../../contexts/GlobalContext";
 import { ModalContext } from "../../contexts/ModalContext";
 
 
-const Dashboard = ({ auth, setAuth }) => {
+const Dashboard = () => {
 
-  const {close, deleted} = useContext(ModalContext)
-  
-  const [loading, setLoading] = useState(false);
-
-
- 
+  const { close, deleted } = useContext(ModalContext)
+  const { loading } = useContext(GlobalContext);
 
   return (
     <DivLoginPrincipal>
       {deleted && (<ModalDelete />)}
       {close && <ModalAdd />}
       {loading && (<BaseLoading><LoadingRing color="#ff577f" /></BaseLoading>)}
-      <Header setLoading={setLoading} setAuth={setAuth} />
+      <Header />
       <SectionDashboard />
       <SectionPrincipal />
     </DivLoginPrincipal>
